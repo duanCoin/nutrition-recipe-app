4019139a4bb472464bb728278ae6744192a496d2
duanCoin <duanJM104@163.com>
2017-05-08 14:55:53 +0800

    final

--- public/src/components/Nutrition.js
+++ public/src/components/Nutrition.js
@@ -1,16 +1,14 @@
 import React from 'react';
-var nutrition = require("../mongo/yingYangShiPu.js");
+var nutrition = require("../mongo/yingYangDaPei.js");
 
 class Nutrition extends React.Component {
 
-  render() {  ///  婴幼儿期、儿童期、青少年期、成年期及老年期
+  render() {
     return <div>
       {nutrition.default.map((item,index)=> {
         return <div key={index}>
-          <h3>{item.date}</h3>
-          <h4>早餐：{item.breakfast}</h4>
-          <h4>午餐：{item.lunch}</h4>
-          <h4>晚餐：{item.supper}</h4>
+          <h3>{item.stage}</h3>
+          <h4>{item.collocation}</h4>
         </div>
       })}
     </div>
